b5d7da6ce1e9d18657b49db96992fefd2eb8c04f
Harsh Shandilya <harsh@prjkt.io>
2019-04-26 05:06:09 +0530

    Prepare for Drone deployment
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- app/test_assets.sh
+++ app/test_assets.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-find src/main/assets/overlays -type f -iname '*.xml' -exec xmlstarlet val {} \; | grep invalid
+if [ ! -z "$(command -v xmlstarlet)" ]; then
+    find src/main/assets/overlays -type f -iname '*.xml' -exec xmlstarlet val {} \; | grep invalid
+else
+    exit 0
+fi
